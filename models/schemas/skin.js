2dd5ea27158923cf79bf01e0c43c24fb22136dff
Jose <angel.caballep@gmail.com>
2019-04-08 03:31:48 -0400

    All models that we will need have been added

--- models/schemas/skin.js
+++ models/schemas/skin.js
@@ -2,8 +2,8 @@
 
 const mongoose = require("mongoose");
 
-const abilities = mongoose.model("ability", new mongoose.Schema({
-    _id: Schema.Types.ObjectId,
+const skinSchema = mongoose.model("skin", new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     name: String,
     splashArtPath: String,
     champion: [{type: Schema.Types.ObjectId, ref: "champion"}]
--- models/schemas/user.js
+++ models/schemas/user.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const userSchema = Schema ({
-    _id: Schema.Types.ObjectId,
-    user_name:String,
-    password:String,
-    avatar_url:String,
-    abilities: [{type: Schema.Types.ObjectId, ref: "ability"}],
-    skins: [{type: Schema.Types.ObjectId, ref: "skin"}]
-});
+const userSchema = mongoose.model("user", new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userName: String,
+    userFirstName: String,
+    userLastName: String,
+    userAge: Number,
+    password: String,
+    avatar_url: String,
+    about: String,
+    userFavoriteChampions: [{type: Schema.Types.ObjectId, ref: "userFavoriteChampions"}]
+}));
